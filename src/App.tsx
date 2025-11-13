d6e78144b68db7fba927524b199382ee111c924a
KimGenius1214 <dzdiz45@gmail.com>
2025-11-13 10:33:52 +0900

    Feat (init): solid 프로젝트 공부

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import type { Component } from "solid-js";
+import Counter from "./Test";
+
+const App: Component = () => {
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
