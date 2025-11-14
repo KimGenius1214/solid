851fabdd17360fbdc06d146e1593a673c20b3d48
KimGenius1214 <dzdiz45@gmail.com>
2025-11-14 10:52:08 +0900

    Feat (navigation): Add button to navigate to counter with dynamic route

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
-import { A } from "@solidjs/router";
+import { A, useNavigate } from "@solidjs/router";
 import type { JSX } from "solid-js";
 
-const App = (props: { children?: JSX.Element }) => (
-  <>
-    <nav style={{ padding: "20px", background: "#f0f0f0" }}>
-      <A href="/" style={{ "margin-right": "20px" }}>
-        Home
-      </A>
-      <A href="/counter">Counter</A>
-    </nav>
-    <h1>Site Title</h1>
-    <main style={{ padding: "20px" }}>{props.children}</main>
-  </>
-);
+const App = (props: { children?: JSX.Element }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/counter/1");
+  };
+  return (
+    <>
+      <nav style={{ padding: "20px", background: "#f0f0f0" }}>
+        <A href="/" style={{ "margin-right": "20px" }}>
+          Home
+        </A>
+        <A href="/counter">Counter</A>
+      </nav>
+      <h1>Site Title</h1>
+      <main style={{ padding: "20px" }}>{props.children}</main>
+      <button type="button" onClick={handleClick}>
+        Navigate to Counter
+      </button>
+    </>
+  );
+};
 
 export default App;
